79ed82cfe1e7e1685fd86d709956a901e778775c
장동완 <jangdongwan0313@almond1.co.kr>
2025-09-19 14:08:25 +0900

    feat: first commit

---
+++ app/src/main/java/com/jdw/random_lotto/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.jdw.random_lotto.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+
+val primaryLight = Color(0xFF006FFD)
+val primaryDark = Color(0xFF90CAF9)
+
+val secondaryLight = Color(0xFF4FC3F7)
+val secondaryDark = Color(0xFF1E88E5)
+
+val tertiaryLight = Color(0xFFFFC107)
+val tertiaryDark = Color(0xFFFFB300)
