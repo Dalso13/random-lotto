dbe6dc1c7ccced9a0069e83d251604468ca37cb4
장동완 <jangdongwan0313@almond1.co.kr>
2025-11-14 18:23:11 +0900

    refactor: 태마 ui 수정

--- app/src/main/java/com/jdw/random_lotto/presentation/main/components/ThemeSettingDialog.kt
+++ app/src/main/java/com/jdw/random_lotto/presentation/main/components/ThemeSettingDialog.kt
@@ -1,13 +1,14 @@
 package com.jdw.random_lotto.presentation.main.components
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -15,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.jdw.random_lotto.common.util.ThemeMode
@@ -33,21 +35,26 @@ fun ThemeSettingDialog(
         containerColor = cs.background,
         text = {
             Column {
-                Text(text = "테마 설정")
+                Text(text = "테마 설정", style = MaterialTheme.typography.titleMedium)
+                Spacer(modifier = Modifier.padding(8.dp))
                 Spacer(modifier = Modifier.width(8.dp))
-                Row {
+                Column {
                     ThemeMode.entries.forEach { mode ->
-                        FilterChip(
-                            selected = (selected == mode),
-                            onClick = { selected = mode },
-                            label = { Text(text = mode.title) },
-                            colors = FilterChipDefaults.filterChipColors(
-                                containerColor = cs.surfaceVariant,
-                                labelColor = cs.onSurfaceVariant,
-                                selectedContainerColor = cs.primaryContainer,
-                                selectedLabelColor = cs.onPrimaryContainer,
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .clickable {
+                                   selected = mode
+                                }
+                                .padding(end = 16.dp)
+                        ) {
+                            Text(text = mode.title)
+                            Spacer(modifier = Modifier.weight(1f))
+                            RadioButton(
+                                selected = (selected == mode),
+                                onClick = { selected = mode },
                             )
-                        )
+                        }
                         Spacer(modifier = Modifier.width(8.dp))
                     }
                 }
