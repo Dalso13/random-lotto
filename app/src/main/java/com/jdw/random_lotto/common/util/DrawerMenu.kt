58c72f21d1923474ee2c7e176af3c10c7b5bdb81
장동완 <jangdongwan0313@almond1.co.kr>
2025-11-14 18:22:39 +0900

    feat: 드로어 메뉴 ui 수정

--- app/src/main/java/com/jdw/random_lotto/common/util/DrawerMenu.kt
+++ app/src/main/java/com/jdw/random_lotto/common/util/DrawerMenu.kt
@@ -1,12 +1,24 @@
 package com.jdw.random_lotto.common.util
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.History
+import androidx.compose.material.icons.outlined.Palette
+import androidx.compose.ui.graphics.vector.ImageVector
+
 /**
  * 네비게이션 드로어 메뉴
  * @param title 메뉴 타이틀
  */
 enum class DrawerMenu(
-    val title: String
+    val title: String,
+    val icon: ImageVector
 ) {
-    HISTORY("당첨 기록"),
-    THEME("테마 설정"),
+    THEME(
+        title = "테마 설정",
+        icon = Icons.Outlined.Palette
+    ),
+    HISTORY(
+        title = "당첨 기록",
+        icon = Icons.Outlined.History
+    ),
 }
